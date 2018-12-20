bf88c57aafbbc6d216fe49fd855b44b8ffe5bb0f
mounikagotham <mounika.gotham@nationwidedigital.co.uk>
2018-12-20 15:41:38 +0000

    adding the microservice-two metrics

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const pkg = require ('./package.json')
 const PORT = process.env.PORT || 8001
 const log = require('./lib/log')
 
+var appzip = require('appmetrics-zipkin')({
+    host: '10.0.207.186',
+    port: 9411,
+    serviceName:pkg.name,
+    sampleRate: 1.0
+  });
+
 server.listen(PORT, () => {
     log().info(`${pkg.name}is running on port ${PORT}`)
 })
